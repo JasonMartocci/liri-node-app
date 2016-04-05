52151fb5fdc4f92151d239e6653cddc27f30b848
Jason Martocci <martoccijason@gmail.com>
2016-04-05 17:07:59 -0400

    twitter working

--- liri.js
+++ liri.js
@@ -6,9 +6,8 @@ var twitterKeys = apiKeys.twitterKeys;
 var Twitter = require('twitter');
 
 // this function logs strings both to the console and to log.txt
-function conAndFileLog(str) {
+function consoleFileLog(str) {
 	console.log(str);
-	// fs.appendFile("log.txt", str + "\n");
 }
 
 // Takes in all of the command line arguments
@@ -23,37 +22,37 @@ if (selections == "my-tweets"){
 	myTweets();
 	function myTweets() {
 
-	var client = twitterKeys;
+		var client = twitterKeys;
 
-	// define the parameters of our call
-	var params = {screen_name: 'jasonmartocci'};
+		// define the parameters of our call
+		var params = {screen_name: 'jasonmartocci'};
 
-	// send out the call to the Twitter API
-	client.get('statuses/user_timeline', params, function(error, timeline, response) {
-		
-		// if no errors... 
-		if (!error) {
-			for (tweet in timeline) {
-				if (tweet < 10) { 
-					// get the date of the tweet
-					var tweetDate = new Date(timeline[tweet].created_at);
+		// send out the call to the Twitter API
+		client.get('statuses/user_timeline', params, function(error, timeline, response) {
+			
+			// if no errors... 
+			if (!error) {
+				for (tweet in timeline) {
+					if (tweet < 9) { 
+						// get the date of the tweet
+						var tweetDate = new Date(timeline[tweet].created_at);
 
-					// log out the date and text of our latest tweets.
-					conAndFileLog("Tweet #" + (parseInt(tweet) + 1) + " // Date: " + tweetDate.toString().slice(0, 24)); // keep only important date info
-					conAndFileLog(timeline[tweet].text);
-				}
-				else {
-					return true;
+						// log out the date and text of our latest tweets.
+						consoleFileLog("Tweet #" + (parseInt(tweet) + 1) + " // Date: " + tweetDate.toString().slice(0, 24)); 
+						consoleFileLog(timeline[tweet].text);
+					}
+					else {
+						return true;
+					}
 				}
 			}
-		}
-		// If there was an error, though
-		else {
-			// log it to the console.
-			conAndFileLog(error);
-		}
-	});
-}
+			else {
+				// log it to the console.
+				consoleFileLog(error);
+			}
+		});
+	}
+
 } else if (selections == "spotify-this-song"){
 	console.log("Spotify");
 } else if (selections == "movie-this"){
