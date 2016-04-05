3fe4213cab5a3997ed80cfe11242a7f0f1b0b392
Jason Martocci <martoccijason@gmail.com>
2016-04-05 17:30:03 -0400

    starting on spotify

--- liri.js
+++ liri.js
@@ -4,8 +4,9 @@ var apiKeys = require("./keys.js");
 var twitterKeys = apiKeys.twitterKeys;
 
 var Twitter = require('twitter');
+var Spotify = require('spotify');
 
-// this function logs strings both to the console and to log.txt
+// this function logs to the console
 function consoleFileLog(str) {
 	console.log(str);
 }
@@ -15,48 +16,51 @@ var inputString = process.argv;
 
 // Parses the command line argument to capture the data
 var selections = inputString[2];
+var argumentOne = process.argv[3];
+var argumentTwo = process.argv[4];
 
 
 // Based on the selections we run the appropriate if statement
 if (selections == "my-tweets"){
 	myTweets();
-	function myTweets() {
-
-		var client = twitterKeys;
-
-		// define the parameters of our call
-		var params = {screen_name: 'jasonmartocci'};
-
-		// send out the call to the Twitter API
-		client.get('statuses/user_timeline', params, function(error, timeline, response) {
-			
-			// if no errors... 
-			if (!error) {
-				for (tweet in timeline) {
-					if (tweet < 9) { 
-						// get the date of the tweet
-						var tweetDate = new Date(timeline[tweet].created_at);
-
-						// log out the date and text of our latest tweets.
-						consoleFileLog("Tweet #" + (parseInt(tweet) + 1) + " // Date: " + tweetDate.toString().slice(0, 24)); 
-						consoleFileLog(timeline[tweet].text);
-					}
-					else {
-						return true;
-					}
-				}
-			}
-			else {
-				// log it to the console.
-				consoleFileLog(error);
-			}
-		});
-	}
-
 } else if (selections == "spotify-this-song"){
-	console.log("Spotify");
+	mySpotify();
 } else if (selections == "movie-this"){
 	console.log("OMDB");
 } else if (selections == "do-what-it-says"){
 	console.log("Do what it says.");
-} 
+}
+
+// Twitter function 
+function myTweets() {
+	var client = twitterKeys;
+	var params = {screen_name: 'jasonmartocci'};
+
+	// send out the call to the Twitter API
+	client.get('statuses/user_timeline', params, function(error, timeline, response) {
+		
+		if (!error) {
+			for (tweet in timeline) {
+				if (tweet < 9) { 
+					// get the date of the tweet
+					var tweetDate = new Date(timeline[tweet].created_at);
+
+					// log out the date and text of our latest tweets.
+					consoleFileLog("Tweet #" + (parseInt(tweet) + 1) + " // Date: " + tweetDate.toString().slice(0, 24)); 
+					consoleFileLog(timeline[tweet].text);
+				}
+				else {
+					return true;
+				}
+			}
+		}
+		else {
+			consoleFileLog(error);
+		}
+	});
+}
+
+// Spotify function
+function mySpotify() {
+	console.log("Spotify");
+};
