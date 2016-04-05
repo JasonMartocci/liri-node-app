610d898e536811802bdfe67d53e380069974ce45
Jason Martocci <martoccijason@gmail.com>
2016-04-05 17:02:43 -0400

    twitter working

--- liri.js
+++ liri.js
@@ -1,19 +1,15 @@
 // Makes connection to the keys file
 var apiKeys = require("./keys.js");
 
+var twitterKeys = apiKeys.twitterKeys;
+
 var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: 'FDvRzy6S3u2eGD0Q5ao8L36FQ',
-  consumer_secret: '3kDtvZvXS09A6mTS1ypABKAjd7853pAKYcPLLAyyJM9SMFaG6F',
-  access_token_key: '2331033409-76NYiYct9DHoC2Kzgc3O5xnEfmraQIfKkFqx7Pr',
-  access_token_secret: 'dda5LLQOZ2ZDnDjozcaVPUXWog4ecfIh4t28rv49Z77n5',
-});
 
-var params = {screen_name: 'jasonmartocci'};
-
-// Run functions in the keys file
-// apiKeys.world();
+// this function logs strings both to the console and to log.txt
+function conAndFileLog(str) {
+	console.log(str);
+	// fs.appendFile("log.txt", str + "\n");
+}
 
 // Takes in all of the command line arguments
 var inputString = process.argv;
@@ -21,13 +17,43 @@ var inputString = process.argv;
 // Parses the command line argument to capture the data
 var selections = inputString[2];
 
+
 // Based on the selections we run the appropriate if statement
 if (selections == "my-tweets"){
-	client.get('statuses/user_timeline', params, function(error, tweets, response){
-	  if (!error) {
-	    console.log(tweets);
-	  }
+	myTweets();
+	function myTweets() {
+
+	var client = twitterKeys;
+
+	// define the parameters of our call
+	var params = {screen_name: 'jasonmartocci'};
+
+	// send out the call to the Twitter API
+	client.get('statuses/user_timeline', params, function(error, timeline, response) {
+		
+		// if no errors... 
+		if (!error) {
+			for (tweet in timeline) {
+				if (tweet < 10) { 
+					// get the date of the tweet
+					var tweetDate = new Date(timeline[tweet].created_at);
+
+					// log out the date and text of our latest tweets.
+					conAndFileLog("Tweet #" + (parseInt(tweet) + 1) + " // Date: " + tweetDate.toString().slice(0, 24)); // keep only important date info
+					conAndFileLog(timeline[tweet].text);
+				}
+				else {
+					return true;
+				}
+			}
+		}
+		// If there was an error, though
+		else {
+			// log it to the console.
+			conAndFileLog(error);
+		}
 	});
+}
 } else if (selections == "spotify-this-song"){
 	console.log("Spotify");
 } else if (selections == "movie-this"){
