8f892a5f4a7cd8461de3c9adae383c1318530c5f
Jason Martocci <martoccijason@gmail.com>
2016-04-05 14:04:41 -0400

    started on twitter api

--- liri.js
+++ liri.js
@@ -1,6 +1,17 @@
 // Makes connection to the keys file
 var apiKeys = require("./keys.js");
 
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: 'FDvRzy6S3u2eGD0Q5ao8L36FQ',
+  consumer_secret: '3kDtvZvXS09A6mTS1ypABKAjd7853pAKYcPLLAyyJM9SMFaG6F',
+  access_token_key: '2331033409-76NYiYct9DHoC2Kzgc3O5xnEfmraQIfKkFqx7Pr',
+  access_token_secret: 'dda5LLQOZ2ZDnDjozcaVPUXWog4ecfIh4t28rv49Z77n5',
+});
+
+var params = {screen_name: 'jasonmartocci'};
+
 // Run functions in the keys file
 // apiKeys.world();
 
@@ -12,7 +23,11 @@ var selections = inputString[2];
 
 // Based on the selections we run the appropriate if statement
 if (selections == "my-tweets"){
-	console.log("Twitter");
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+	  if (!error) {
+	    console.log(tweets);
+	  }
+	});
 } else if (selections == "spotify-this-song"){
 	console.log("Spotify");
 } else if (selections == "movie-this"){
